5ab58e08a0f556ef15ea88e1f8783df25a61c87d
Joe FitzPatrick <joefitz@securinghardware.com>
2021-10-07 17:21:32 -0700

    user 1000 since user pi isn't defined

--- stage2/04-hardhat/00-run.sh
+++ stage2/04-hardhat/00-run.sh
@@ -1,4 +1,4 @@
-install -m 644 -o pi files/jtagenabler.cpp "${ROOTFS_DIR}/home/pi/jtagenabler.cpp"
+install -m 644 -o 1000 files/jtagenabler.cpp "${ROOTFS_DIR}/home/pi/jtagenabler.cpp"
 install -m 644 files/hardhat/software/pah.py "${ROOTFS_DIR}/root/pah.py"
 install -m 755 files/hardhat.sh "${ROOTFS_DIR}/root/hardhat.sh"
 install -m 644 files/hardhat.service "${ROOTFS_DIR}/etc/systemd/system/hardhat.service"
